dee666add5e0a8ded0f1b2068be3d59a3eee53dd
Krishna Pradeep Dawalkar <krishna3dawalkar@gmail.com>
2020-10-14 21:22:54 +0530

    For loops updated

---
+++ for_loop.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+for(int i=0;i<3;i++){
+    cout<<"Hi"<<endl;
+}
+cout<<"Done with For looops"<<endl;
+cout<<"What about while loops"<<endl;
+return 0;
+}
